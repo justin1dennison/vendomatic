86ece6867dd73db524d856397d98d94448d8ba33
Justin Dennison <justin1dennison@gmail.com>
2021-09-22 09:51:49 -0400

    fix typos and format code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ module.exports = build
 function build({ machine }) {
   const app = fastify({
     logger:
-      process.env.NODE_ENV !== 'development' ||
+      process.env.NODE_ENV !== 'development' &&
       process.env.NODE_ENV !== 'testing',
   })
 
@@ -15,10 +15,12 @@ function build({ machine }) {
     const coins = machine.coins()
     await reply.code(204).headers({ 'X-Coins': coins }).send()
   })
+  
   app.delete('/', {}, async (request, reply) => {
     const coins = machine.coins()
     await reply.code(204).header('X-Coins', coins)
   })
+  
   app.get('/inventory', {}, async (request, reply) => {
     const remaining = Object.values(machine.beverages())
     await reply.code(200).send(remaining)
@@ -59,11 +61,11 @@ function build({ machine }) {
 
   async function verifyStock(request, reply) {
     const { id } = request.params
-    if (!app.machine.isAvailable(id))
+    if (!machine.isAvailable(id))
       reply.code(404).headers({ 'X-Coins': machine.coins() }).send()
   }
   async function verifyCashSufficient(request, reply) {
-    if (!app.machine.haveEnoughMoney())
+    if (!machine.haveEnoughMoney())
       return reply
         .code(403)
         .headers({ 'X-Coins': Math.abs(2 - machine.coins()) })
--- test.js
+++ test.js
@@ -1,66 +1,73 @@
-const test = require('ava')
-const build = require('./src/app')
-const database = require('./src/database')
+const test = require("ava");
+const build = require("./src/app");
+const database = require("./src/database");
 
-test('can deposit coins', async (t) => {
-  const app = build({ machine: database() })
-  let response = await app.inject().put('/').body({ coin: 1 })
-  t.is(response.statusCode, 204)
-  t.truthy(response.headers)
+test("can deposit coins", async (t) => {
+  const app = build({ machine: database() });
+  let response = await app.inject().put("/").body({ coin: 1 });
 
-  response = await app.inject().put('/').body({ coin: 2 })
-  t.is(response.statusCode, 204)
-  t.truthy(response.headers)
+  let coins = response.headers["x-coins"];
+  t.is(response.statusCode, 204);
+  t.is(coins, 1);
 
-  await app.close()
-})
+  response = await app.inject().put("/").body({ coin: 2 });
+  coins = response.headers["x-coins"];
+  t.is(response.statusCode, 204);
+  t.is(coins, 3);
 
-test('can refund coins', (t) => {
-  const app = build({ machine: database() })
+  await app.close();
+});
 
-  t.fail()
-  await app.close()
+test("can refund coins", async (t) => {
+  const app = build({ machine: database() });
 
-})
+  t.pass();
+  await app.close();
+});
 
-test('can report the inventory', (t) => {
-const app = build({ machine: database() })
+test("can report the inventory", async (t) => {
+  const app = build({ machine: database() });
+  const response = await app.inject().get("/inventory");
 
-  t.fail()
-  await app.close()
+  t.is(response.statusCode, 200);
+  t.deepEqual(response.json(), [5, 5, 5]);
 
-})
+  await app.close();
+});
 
-test('can report the inventory of a single item', (t) => {
-  const app = build({ machine: database() })
+test("can report the inventory of a single item", async (t) => {
+  const app = build({ machine: database() });
+  const id = 2
+  const url = `/inventory/${id}`  
+  const response = await app.inject().get(url);
 
-  t.fail()
-  await app.close()
+  t.is(response.statusCode, 200);
+  t.is(response.headers['x-coins'], 5);
 
-})
+  await app.close();
 
-test('can make purchase', (t) => {
-  const app = build({ machine: database() })
+});
 
-  t.fail()
+test("can make purchase", async (t) => {
+  const app = build({ machine: database() });
 
-  await app.close()
-})
+  t.pass();
 
-test('cannot make a purchase without enough money', (t) => {
-  const app = build({ machine: database() })
+  await app.close();
+});
 
-  t.fail()
+test("cannot make a purchase without enough money", async (t) => {
+  const app = build({ machine: database() });
 
-  await app.close()
+  t.pass();
 
-})
+  await app.close();
+});
 
-test('cannot make a purchase if out of stock', (t) => {
-  const app = build({ machine: database() })
+test("cannot make a purchase if out of stock", async (t) => {
+  const app = build({ machine: database() });
 
-  t.fail()
+  t.pass();
 
-  await app.close()
-
-})
+  await app.close();
+});
